867f725d02584d003799da705ac152dd87526bbd
Dewansh Yadav <dewanshyadaw8@gmail.com>
2026-02-15 13:07:48 +0530

    Add switch statement example in main.go to demonstrate day of the week output

---
+++ switch/main.go
@@ -0,0 +1,21 @@
+package main
+
+func main() {
+
+	day := 6
+
+	switch day {
+	case 1:
+		println("Monday")
+	case 2:
+		println("Tuesday")
+	case 3:
+		println("Wednesday")
+	case 4:
+		println("Thursday")
+	case 5:
+		println("Friday")
+	default:
+		println("Weekend")
+	}
+}
