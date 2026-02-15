c8ec281a90e0948d3e280b4d4f81e4e9c48caa73
Dewansh Yadav <dewanshyadaw8@gmail.com>
2026-02-15 14:43:31 +0530

    Add main.go to demonstrate type conversion from int to float64 in Go

---
+++ conversion/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int = 42
+	fmt.Println("Number is", num)
+	fmt.Printf("Type of num is %T\n", num)
+	var data float64 = float64(num)
+	fmt.Println("Data is ", data)
+	fmt.Printf("Type of Data is %T\n", data)
+
+}
